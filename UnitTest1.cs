cb7c75c875ef505ab960460d8040123955cc7e66
User <User@LENOVO-T480S>
2023-12-01 18:38:08 +0100

    Add project files.

---
+++ UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace konyvTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
